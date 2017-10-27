6d9e06fedd767020cbcfa2f35f23245dc4a6af90
VALTA\Ozan <ozanacarali@hotmail.co.uk>
2017-10-27 23:36:13 +0100

    Changes to TankPlayerController (Used DeprojectScreenPositionToWorld to find the world direction that the crosshair is pointing in relative to the tank, refactored that code into a seperate GetLookDirection() function). (Video 130)

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -66,8 +66,26 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) cons
 	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
 
 	//"De-project" the screen position of the crosshair to a world direction
+	FVector LookDirection;
+	if (GetLookDirection(ScreenLocation, LookDirection))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("LookDirection: %s"), (*LookDirection.ToString()));
+	}
+
 	//Line-trace along that look direction, and see what we hit (up to max range)
 
 	return true;
 }
 
+
+bool ATankPlayerController::GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const
+{
+	FVector CameraWorldLocation; //To be discarded
+	return DeprojectScreenPositionToWorld
+	(
+		ScreenLocation.X,
+		ScreenLocation.Y,
+		CameraWorldLocation,
+		LookDirection
+	);
+}
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -31,6 +31,9 @@ private:
 	
 	//Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+
+
 
 	UPROPERTY(EditAnywhere)
 	float CrossHairXLocation = 0.5;
