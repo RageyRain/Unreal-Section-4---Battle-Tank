07e1464bc9049690010fa8e0d06c4720453614eb
VALTA\Ozan <ozanacarali@hotmail.co.uk>
2017-10-27 22:00:53 +0100

    Changes to TankPlayerController (Declared and defined GetSightRayHitLocation with out parameter: FVector& OutHitLocation, set up if function that will eventually tell the tank where to aim based on the players crosshair.). (Video 128)

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -24,9 +24,13 @@ public:
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
+
 private:
 	//Start the tank moving the barrel so that a shot would hit where the crosshair intersects with the world
 	void AimTowardsCrosshair();
 	
+	//Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
 	
 };
