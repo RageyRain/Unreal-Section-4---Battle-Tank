c32c5983db16c4e8f2147a59a7e9666dc5003c38
VALTA\Ozan <ozanacarali@hotmail.co.uk>
2017-10-27 22:46:54 +0100

    Changes to TankPlayerController (Moved GetControlledTank() into private catergory, initiated CrossHairX/YLocation variables in the header, Added logic to calculate the pixel coordinate of the crosshair on the screen in GetSightRayHitLocation). (Video 129)

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -50,7 +50,7 @@ void ATankPlayerController::AimTowardsCrosshair()
 	if (GetSightRayHitLocation(HitLocation)) // Has "side-effect", is going to line trace
 	{
 
-		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), (*HitLocation.ToString()));
+		//UE_LOG(LogTemp, Warning, TEXT("Look direction: %s"), (*HitLocation.ToString()));
 
 		// TODO Tell controlled tank to aim at this point
 	}
@@ -60,7 +60,14 @@ void ATankPlayerController::AimTowardsCrosshair()
 //Get world location of linetrace through crosshair, true if hits landscape
 bool ATankPlayerController::GetSightRayHitLocation(FVector& OutHitLocation) const
 {
-	OutHitLocation = FVector(1.0);
+	//Find the crosshair position in pixel coordinates
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+
+	//"De-project" the screen position of the crosshair to a world direction
+	//Line-trace along that look direction, and see what we hit (up to max range)
+
 	return true;
 }
 
--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -19,18 +19,22 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	ATank* GetControlledTank() const;
-
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 
 
 private:
+	ATank* GetControlledTank() const;
+
 	//Start the tank moving the barrel so that a shot would hit where the crosshair intersects with the world
 	void AimTowardsCrosshair();
 	
 	//Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
 
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.33333;
 	
 };
