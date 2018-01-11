c275d7d0418c3d631073202becb38d968f61c5f4
VALTA\Ozan <ozanacarali@hotmail.co.uk>
2018-01-11 12:59:56 +0000

    (Video 160) -Commented out default sub object setting of "TankMovementComponent" int "Tank.cpp" -Manually re-added "TankMovementComponent" to "Tank_BP"; added new reference to "TankMovementComponent" to plug into "IntendMoveForward" node -Added pointers to "LeftTrack" and "RightTrack" in "TankMovementComponent" header file -Added "Initialise" function to "TankMovementComponent"; added references and protections for each track -Added "Initialise" function as node to "Tank_BP" event graph and plugged in references to each component -Made "TankMovementComponent" "BlueprintSpawnable" -Added code to "IntendMoveForward" that assigns the "Throw" value from the "Move Forward" input-axis to each track

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -17,7 +17,7 @@ ATank::ATank()
 
 	// No need to protect pointers as added at construction
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("Aiming Component"));
-	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
+	//TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("Movement Component"));
 }
 
 // Called when the game starts or when spawned
--- BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankMovementComponent.cpp
@@ -1,12 +1,28 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankMovementComponent.h"
+#include "TankTrack.h"
+
+void UTankMovementComponent::Initialise(UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
+{
+	if (!LeftTrackToSet || !RightTrackToSet) { return; }
+	LeftTrack = LeftTrackToSet;
+	RightTrack = RightTrackToSet;
+	
+}
+
+
 
 void UTankMovementComponent::IntendMoveForward(float Throw)
 {
 	//auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("Intend move forward throw: %f"), Throw);
 
+	LeftTrack->SetThrottle(Throw);
+	RightTrack->SetThrottle(Throw);
+
+
+	// TODO prevent double-speed due to dual control use
 }
 
 
