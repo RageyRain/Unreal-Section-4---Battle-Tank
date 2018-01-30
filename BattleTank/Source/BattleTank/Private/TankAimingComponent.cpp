4e08779a954e7e760290acb336ab4398e131d85f
VALTA\Ozan <ozanacarali@hotmail.co.uk>
2018-01-30 14:27:34 +0000

    (Video 180) -Ran into problems and had to rebuild project -Added "BeginPlay()" and "TickComponent" to "TankAimingComponent" -Made the default "FiringState" of the tank "Reloading" -Set "BeginPlay" to act as if the tank has fired to reset the reload timer in "TankAimingComponent.cpp" -Changed logic for "Fire()" to rely on the ENum "FiringState" instead of it's own "isReloaded" variable -Moved ensure for "Barrel" and "ProjectileBlueprint" into firing if loop so that it's called right when it's needed

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -20,11 +20,28 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = false;
+	PrimaryComponentTick.bCanEverTick = true;
 
 	// ...
 }
 
+void UTankAimingComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	//So that first fire is after initial reload
+	LastFireTime = FPlatformTime::Seconds();
+
+}
+
+
+void UTankAimingComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+	if ((FPlatformTime::Seconds() - LastFireTime) > ReloadTimeInSeconds)
+	{
+		FiringState = EFiringState::Reloading;
+	}
+	// TODO handle aiming and locked states
+}
 
 void UTankAimingComponent::AimAt(FVector OutHitLocation)
 
@@ -75,10 +92,10 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 
 void UTankAimingComponent::Fire()
 {
-	if (!ensure(Barrel && ProjectileBlueprint)) { return; }
-	bool isReloaded = (FPlatformTime::Seconds() - LastFireTime) > ReloadTimeInSeconds;
-	if (isReloaded)
+	if (FiringState != EFiringState::Reloading)
 	{
+		if (!ensure(Barrel)) { return; }
+		if (!ensure(ProjectileBlueprint)) { return; }
 		//Spawn a projectile at the socket location on the barrel
 		auto Projectile = GetWorld()->SpawnActor<AProjectile>(
 			ProjectileBlueprint,
