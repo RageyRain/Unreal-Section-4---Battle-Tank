5a453a5aea7bc7718fabeda367d164e4891a4e18
VALTA\Ozan <ozanacarali@hotmail.co.uk>
2018-02-05 14:19:38 +0000

    (Video 183) -Made both "LeftTrack" and "RightTrack" generate hit events on collision in "Tank_BP" -Added code to "TankTrack.cpp" and "TankTrack.h" to detect the collision event between the track and the ground and log it out

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -12,6 +12,13 @@ UTankTrack::UTankTrack()
 
 }
 
+
+void UTankTrack::BeginPlay()
+{
+	Super::BeginPlay();
+	OnComponentHit.AddDynamic(this, &UTankTrack::OnHit);
+}
+
 void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
@@ -42,4 +49,8 @@ void UTankTrack::SetThrottle(float Throttle)
 
 }
 
+void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Track contact"));
+}
 
