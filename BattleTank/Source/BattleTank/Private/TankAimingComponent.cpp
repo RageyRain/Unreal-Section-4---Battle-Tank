2f1308a35955ec79ffc22e5055ac6dd7e9f7c634
VALTA\Ozan <ozanacarali@hotmail.co.uk>
2018-02-08 15:12:10 +0000

    (Video 188) -Imported provided assets for mortar body, turret and barrel -Made an "AutoMortar_BP" blueprint which inherits from the "Pawn" class and set it up in a similar fashion to "Tank_BP" with the mortar assets

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -115,7 +115,7 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	{
 		Turret->Rotate(DeltaRotator.Yaw);
 	}
-	else
+	else //Avoid going the long-way round
 	{
 		Turret->Rotate(-DeltaRotator.Yaw);
 	}
