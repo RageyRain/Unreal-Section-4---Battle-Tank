76e91bdf5a1db9a9abc992fef4ec080b567e7b38
VALTA\Ozan <ozanacarali@hotmail.co.uk>
2018-02-15 14:26:20 +0000

    (Video 195) -Made root component of "Projectile_BP" into "ImpactBlast" and destroyed visible mesh "CollisionMesh" on impact in "Projetile.cpp" -Added timer to spawned projectile to destroy them 5 seconds after impact with code in "Projectile.cpp"

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -56,5 +56,16 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, U
 	LaunchBlast->Deactivate();
 	ImpactBlast->Activate();
 	ExplosionForce->FireImpulse();
+
+	SetRootComponent(ImpactBlast);
+	CollisionMesh->DestroyComponent();
+
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &AProjectile::OnTimerExpire, DestroyDelay, false);
+
 }
 
+void AProjectile::OnTimerExpire()
+{
+	Destroy();
+}
