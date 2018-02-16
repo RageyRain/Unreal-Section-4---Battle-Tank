19c8ad896942e7a2ba9cd3453bbbd45bb84875f6
VALTA\Ozan <ozanacarali@hotmail.co.uk>
2018-02-16 14:50:56 +0000

    (Video 197) -Added "GetHealthPercent()" function to "Tank.cpp" and made it cast "CurrentHealth" and "StartingHealth" into floats and divide them to determine the percentage of remaining health -Created "HealthBar" widget blueprint, gave it blueprint variable "MyTank" and made it return the float from "GetHealthPercent()" defined in "Tank.cpp" -Added widget component to "Tank_BP" and named it "Health Bar" -Initialised "Health Bar" widget by casting "Health Bar" and tank with "self" node to "HealthBar" widget -Resized widget in viewport of "Tank_BP" and set "User Interface -> Space" to "Screen" to render the health in bar in screen space rather than world space

--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -14,14 +14,19 @@ class BATTLETANK_API ATank : public APawn
 {
 	GENERATED_BODY()
 
-	// Sets default values for this pawn's properties
-	ATank();
 
 	public:
 		//Called by the engine when actor damage is dealth
 		virtual float TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, class AController* EventInstigator, class AActor* DamageCauser) override;
+		
+		//Returns current health as a percentage oif starting health, between 0 and 1
+		UFUNCTION(BlueprintPure, Category = "Health")
+		float GetHealthPercent() const;
 
 	private:
+		// Sets default values for this pawn's properties
+		ATank();
+
 		UPROPERTY(EditDefaultsOnly, Category = "Setup")
 		int32 StartingHealth = 100;
 
